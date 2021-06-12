44e6d3c7d502b655b140c7d53732f3f26057c50a
nadavRazT <nadav.raz.41@gmail.com>
2021-06-12 23:04:55 +0300

    not a halbana

--- MapReduceFramework.cpp
+++ MapReduceFramework.cpp
@@ -1,6 +1,10 @@
 #include "MapReduceClient.h"
 #include <pthread.h>
-#include "Barrier/Barrier.h"
+#include <algorithm>
+#include "Barrier/Barrier.cpp"
+#include "atomic"
+#include "iostream"
+
 
 typedef void *JobHandle;
 
@@ -15,16 +19,16 @@ typedef struct {
 
 typedef struct {
     std::atomic<int> *atomic_counter;
-    std::vector<std::pair<K2, V2>> ds2;
-    std::vector<std::pair<K3, V3>> ds3;
-    InputVec &inputVector;
-    MapReduceClient &client;
+    std::vector<std::pair<K2*, V2*>> ds2;
+    std::vector<std::pair<K3*, V3*>> ds3;
+    const InputVec *inputVector;
+    const MapReduceClient *client;
     Barrier* barrier;
 } ThreadContext;
 
 typedef struct {
-    ThreadContext[] thread_contexts;
-    pthread_t[] threads;
+    std::vector<ThreadContext> thread_contexts;
+    std::vector<pthread_t> threads;
     pthread_mutex_t mutex;
     JobState job_state;
     int num_threads;
@@ -32,30 +36,29 @@ typedef struct {
 
 
 void emit2(K2 *key, V2 *value, void *context) {
-    ThreadContext *thread_context = static_cast<ThreadContext>(context);
-    thread_context->ds2.push_back(std::pair<K2, V2>(key, value));
+    ThreadContext *thread_context = static_cast<ThreadContext*>(context);
+    thread_context->ds2.push_back(std::pair<K2*, V2*>(key, value));
 }
 
 void emit3(K3 *key, V3 *value, void *context) {
-    ThreadContext *thread_context = static_cast<ThreadContext>(context);
-    thread_context->ds3.push_back(std::pair<K3, V3>(key, value));
+    ThreadContext *thread_context = static_cast<ThreadContext*>(context);
+    thread_context->ds3.push_back(std::pair<K3*, V3*>(key, value));
 }
 
-void threadMapReduce(void* context)
+void *threadMapReduce(void* context)
 {
-    ThreadContext *thread_context = static_cast<ThreadContext>(context);
+    ThreadContext *thread_context = static_cast<ThreadContext*>(context);
     // map
-    while(thread_context->atomic_counter != inputVec.size())
+    while(reinterpret_cast<unsigned long>(thread_context->atomic_counter) != (thread_context->inputVector)->size() - 1)
     {
         int old_value = (*(thread_context->atomic_counter))++;
-        MapReduceClient &client = thread_context->client;
-        InputVec input_vec = thread_context->inputVector;
-        InputPair input_pair = input_vec[old_value];
-        client.map(input_pair.first, input_pair.second, thread_context);
+        const InputVec *input_vec = thread_context->inputVector;
+        InputPair input_pair = input_vec->at(old_value);
+        thread_context->client->map(input_pair.first, input_pair.second, thread_context);
     }
     // sort
-    std::sort((thread_context->ds2).begin(), (thread_context->ds2).end(), K2::operator<());
-    std::cout<< thread_context->ds2 << std::endl;
+    std::sort((thread_context->ds2).begin(), (thread_context->ds2).end());
+//    std::cout << thread_context->ds2 << std::endl;
 
     thread_context->barrier->barrier();
     // shuffle
@@ -66,19 +69,23 @@ void threadMapReduce(void* context)
 JobHandle startMapReduceJob(const MapReduceClient &client,
                             const InputVec &inputVec, OutputVec &outputVec,
                             int multiThreadLevel) {
-    pthread_t threads[MT_LEVEL];
-    ThreadContext thread_contexts[MT_LEVEL];
+    pthread_t threads[multiThreadLevel];
+    std::vector<ThreadContext> thread_contexts;
+    ThreadContext thread_context_arr[multiThreadLevel];
     std::atomic<int> atomic_counter(0);
     Barrier barrier(multiThreadLevel);
     for (int i = 0; i < multiThreadLevel; ++i) {
-        std::vector<std::pair<K2, V2>> ds2 = new std::vector<K2, V2>();
-        std::vector<std::pair<K3, V3>> ds3 = new std::vector<K3, V3>();
-        thread_contexts[i] = {&atomic_counter, &ds2, &ds3, &inputVec, &client, &barrier};
+        auto *ds2 = new std::vector<std::pair<K2*, V2*>>();
+        auto *ds3 = new std::vector<std::pair<K3*, V3*>>();
+
+        ThreadContext tc = {&atomic_counter, *ds2, *ds3, &inputVec, &client, &barrier};
+        thread_context_arr[i] = tc;
+        thread_contexts.push_back(tc);
     }
 
     for (int i = 0; i < multiThreadLevel; ++i) {
 
-        pthread_create(threads + i, NULL, threadMapReduce, thread_contexts + i);
+        pthread_create(threads + i, NULL, threadMapReduce, thread_context_arr + i);
     }
 
     // wait for job to finish
@@ -86,11 +93,11 @@ JobHandle startMapReduceJob(const MapReduceClient &client,
     // reduce K3 V3 from all threads
 }
 
-void waitForJob(JobHandle job);
+void waitForJob(JobHandle job){};
 
-void getJobState(JobHandle job, JobState *state);
+void getJobState(JobHandle job, JobState *state){};
 
-void closeJobHandle(JobHandle job);
+void closeJobHandle(JobHandle job){};
 
 
 
--- cmake-build-debug/CMakeFiles/os_ex3.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/os_ex3.dir/DependInfo.cmake
@@ -1,7 +1,20 @@
 # The set of languages for which implicit dependencies are needed:
 set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
   )
 # The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/noam/Documents/CLionProjects/OS/os_ex3/MapReduceFramework.cpp" "/home/noam/Documents/CLionProjects/OS/os_ex3/cmake-build-debug/CMakeFiles/os_ex3.dir/MapReduceFramework.cpp.o"
+  "/home/noam/Documents/CLionProjects/OS/os_ex3/SampleClient.cpp" "/home/noam/Documents/CLionProjects/OS/os_ex3/cmake-build-debug/CMakeFiles/os_ex3.dir/SampleClient.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../."
+  "../Barrier"
+  "../Atomic"
+  )
 
 # Targets to which this target links.
 set(CMAKE_TARGET_LINKED_INFO_FILES
